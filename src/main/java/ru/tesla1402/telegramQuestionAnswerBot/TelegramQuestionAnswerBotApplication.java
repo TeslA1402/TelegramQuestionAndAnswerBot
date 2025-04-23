0b14fd0881d07980da47e189b87c848e429a0958
TeslA1402 <ivantimeev95@gmail.com>
2025-04-23 12:39:13 +0500

    Init commit

---
+++ src/main/java/ru/tesla1402/telegramQuestionAnswerBot/TelegramQuestionAnswerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.tesla1402.telegramQuestionAnswerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramQuestionAnswerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramQuestionAnswerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tesla1402/telegramQuestionAnswerBot/TelegramQuestionAnswerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tesla1402.telegramQuestionAnswerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramQuestionAnswerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
